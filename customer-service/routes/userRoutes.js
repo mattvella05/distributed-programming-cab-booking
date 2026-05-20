a075485cecd04d1bdc0af5e6a9393a62347ad42f
Matthew Vella <matthew.vella14@gmail.com>
2026-05-20 09:24:16 +0200

    Implemented user account details endpoint

--- customer-service/routes/userRoutes.js
+++ customer-service/routes/userRoutes.js
@@ -82,4 +82,28 @@ router.post("/login", async (req, res) => {
 
 });
 
+router.get("/:id", async (req, res) => {
+
+    try {
+
+        const user = await User.findById(req.params.id);
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        res.status(200).json(user);
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+
+});
+
 module.exports = router;
