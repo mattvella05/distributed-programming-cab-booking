4a94fb03168092ef9e25ee270d5329ec2289bcf2
Matthew Vella <matthew.vella14@gmail.com>
2026-05-20 09:06:49 +0200

    Implemented user registration endpoint

--- customer-service/routes/userRoutes.js
+++ customer-service/routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const User = require("../models/User");
 
 const router = express.Router();
 
@@ -6,6 +7,44 @@ router.get("/", (req, res) => {
     res.json({
         message: "User Routes Working"
     });
+});
+
+router.post("/register", async (req, res) => {
+
+    try {
+
+        const { firstName, surname, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists"
+            });
+        }
+
+        const newUser = new User({
+            firstName,
+            surname,
+            email,
+            password
+        });
+
+        await newUser.save();
+
+        res.status(201).json({
+            message: "User registered successfully",
+            user: newUser
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+
 });
 
 module.exports = router;
